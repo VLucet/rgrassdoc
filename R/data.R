84ad828edf9f1041a22517cb71f97c5ed7a4afa2
VLucet <valentin.lucet@gmail.com>
2020-05-28 18:30:27 -0400

    add data

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' GRASS 7.8 modules names and descriptions
+#'
+#' A dataset containing the names and descriptions of all GRASS 7.8 modules
+#' scraped from the GRASS manual full index page.
+#'
+#' @format A data frame with 528 rows and 2 columns.
+#' \describe{
+#'   \item{module}{module name}
+#'   \item{description}{module description}
+#' }
+#' @source \url{https://grass.osgeo.org/grass78/manuals/full_index.html}
+"grassmodules78"
