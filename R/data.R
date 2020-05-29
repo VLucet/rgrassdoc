b84d0feaff540731fcc0a192a6bc0ca116fd9d13
VLucet <valentin.lucet@gmail.com>
2020-05-29 12:41:44 -0400

    mention addons in data doc

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
 #' GRASS 7.8 modules names and descriptions
 #'
 #' A dataset containing the names and descriptions of all GRASS 7.8 modules
-#' scraped from the GRASS manual full index page.
+#' and addons, scraped from the GRASS manual full index page.
 #'
 #' @format A data frame with 528 rows and 2 columns.
 #' \describe{
@@ -9,5 +9,7 @@
 #'   \item{description}{character, module description}
 #'   \item{addon}{logical, whether the module is an addon}
 #' }
-#' @source \url{https://grass.osgeo.org/grass78/manuals/full_index.html}
+#' @source
+#' \url{https://grass.osgeo.org/grass78/manuals/full_index.html}
+#' \url{https://grass.osgeo.org/grass78/manuals/addons/}
 "grassmodules78"
