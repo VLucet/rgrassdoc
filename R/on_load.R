fbbfc6afe25b54632a5974031224476f7980583a
Valentin <valentin.lucet@gmail.com>
2021-03-13 13:52:00 -0500

    udpate to grass 7.9

--- R/on_load.R
+++ R/on_load.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname){
-  options(grass.version=7.8, grass.viewer="viewer")
+  options(grass.version=7.9, grass.viewer="viewer")
   packageStartupMessage(paste0("GRASS GIS documentation - set to GRASS version ",
                                (.Options$grass.version)))
 }
