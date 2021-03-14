0379f8c51bc164dbce416bd1b822bf0c99e47b78
Valentin <valentin.lucet@gmail.com>
2021-03-14 10:17:10 -0400

    use cli on load

--- R/on_load.R
+++ R/on_load.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname){
   options(grass.version=7.8, grass.viewer="viewer")
-  packageStartupMessage(paste0("GRASS GIS documentation - set to GRASS version ",
-                               (.Options$grass.version)))
+  cli::cli_alert_info(paste0("GRASS GIS documentation - set to GRASS version ",
+                             cli::col_green(.Options$grass.version)))
 }
